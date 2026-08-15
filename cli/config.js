81ed2d50a68031001af58325f0dbe0d8d4ac53e3
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-15 14:22:44 +0200

    Create config.js

---
+++ cli/config.js
@@ -0,0 +1,18 @@
+// cli/config.js
+
+export const aufbau = {
+  elements: {
+    load: 'auto',
+  },
+};
+
+export const terminal = {
+  cursorBlink: true,
+  fontFamily: 'Menlo, Monaco, "Courier New", monospace',
+  fontSize: 14,
+  theme: {
+    background : '#0f1419',
+    foreground : '#e6edf3',
+    cursor     : '#3fb950'
+  },
+};
