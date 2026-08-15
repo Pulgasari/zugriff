eee02fba9adb36f491958e8c9823d5fced05303b
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-15 14:43:43 +0200

    Create app.js

---
+++ apps/template/app.js
@@ -0,0 +1,18 @@
+// app.js
+
+// :::::: IMPORTS :::::::::::::::::::::::::::::::::::::::::::
+
+// ::: app
+import * as app    from './../shared/js/app.js';
+import * as config from './config.js';
+import { vfs }     from './../shared/js/vfs.js';
+
+// ::: vendors
+import aufbau, { html, dom, preact } from '@aufbau/kits/preact-htm';
+
+// :::::: CONFIG ::::::::::::::::::::::::::::::::::::::::::::
+
+aufbau.init();
+app.adoptStyleSheets(app.css);
+
+// :::::: APP :::::::::::::::::::::::::::::::::::::::::::::::
