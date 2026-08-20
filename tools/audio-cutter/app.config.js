6174a44b65a5c119d181bd4c3eff291879d26aec
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-20 06:30:17 +0200

    Delete tools/audio-cutter/app.config.js

--- tools/audio-cutter/app.config.js
+++
@@ -1,9 +0,0 @@
-// tools/audio-cutter/app.config.js
-
-import { appMeta } from './../registry.js';
-
-export const app = appMeta('audio-cutter');
-
-export const aufbau = {
-  elements : { mode: 'auto' },
-};
