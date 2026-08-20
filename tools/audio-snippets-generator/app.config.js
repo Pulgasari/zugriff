5aaa045361398efaf4e807e60106bfa1ebb10dda
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-20 06:32:23 +0200

    Delete tools/audio-snippets-generator/app.config.js

--- tools/audio-snippets-generator/app.config.js
+++
@@ -1,9 +0,0 @@
-// tools/audio-snippets-generator/app.config.js
-
-import { appMeta } from './../registry.js';
-
-export const app = appMeta('audio-snippets-generator');
-
-export const aufbau = {
-  elements : { mode: 'auto' },
-};
