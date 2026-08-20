8c43e8be9114488e43643b78955a89fe96b70f01
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-20 06:19:41 +0200

    Delete tools/audio-converter/app.config.js

--- tools/audio-converter/app.config.js
+++
@@ -1,9 +0,0 @@
-// tools/audio-converter/app.config.js
-
-import { appMeta } from './../registry.js';
-
-export const app = appMeta('audio-converter');
-
-export const aufbau = {
-  elements : { mode: 'auto' },
-};
