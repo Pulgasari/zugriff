a41b6a140da047cb37284fd2d819838647c95b7c
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-20 06:18:23 +0200

    Delete tools/image-batch-processor/app.config.js

--- tools/image-batch-processor/app.config.js
+++
@@ -1,9 +0,0 @@
-// tools/image-batch-processor/app.config.js
-
-import { appMeta } from './../registry.js';
-
-export const app = appMeta('image-batch-processor');
-
-export const aufbau = {
-  elements : { mode: 'auto' },
-};
