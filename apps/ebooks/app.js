9831ea0673424e4098f7ba62a19630eb5d8d5419
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-29 01:52:41 +0200

    Update app.js

--- apps/ebooks/app.js
+++ apps/ebooks/app.js
@@ -6,14 +6,14 @@
 import { html, signal, computed, useEffect, useRef } from '@aufbau/kits/preact-htm';
 
 // ::: shared
-import { boot, config } from './../../shared/js/app.js?slug=ebooks';
-import { Icon, AppSettings } from './../../shared/js/components/index.js';
-import { stored } from './../../shared/js/lib/signals.js';
-import * as fs    from './../../shared/js/filesystem/fsaccess.js';
-import * as pwa   from './../../shared/js/lib/pwa.js';
+import { boot, config }      from '/.shared/js/app.js?slug=ebooks';
+import { Icon, AppSettings } from '/.shared/js/components/index.js';
+import { stored }            from '/.shared/js/lib/signals.js';
+import * as fs               from '/.shared/js/filesystem/fsaccess.js';
+import * as pwa              from '/.shared/js/lib/pwa.js';
 
 // ::: local
-import * as db     from './db.js';
+import * as db                               from './db.js';
 import { createPdfReader, createEpubReader } from './reader.js';
 
 // :::::: STATE ::::::::::::::::::::::::::::::::::::::::::::::
