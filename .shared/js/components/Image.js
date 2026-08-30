96eb3d2531a406937ad289d13c63b393053d0aab
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-30 18:45:50 +0200

    Update Image.js

--- .shared/js/components/Image.js
+++ .shared/js/components/Image.js
@@ -1,6 +1,10 @@
+// components/Image.js
 
 import { html } from '@aufbau/kits/preact-htm';
 
-export default function ({ alt='' loading = 'lazy', src )} {
+function ({ alt='' loading = 'lazy', src )} {
   html`<img src=${src} alt=${alt} loading=${loading} />`;
 }
+
+export       { Image };
+export default Image;
