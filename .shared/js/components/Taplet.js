ae5ff04a5635d217ceafbbc78fae7b0e9ae198c8
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-30 19:03:03 +0200

    Update Taplet.js

--- .shared/js/components/Taplet.js
+++ .shared/js/components/Taplet.js
@@ -1,12 +1,15 @@
-// shared/js/components/Taplet.js
+// components/Taplet.js
 
 import { html } from '@aufbau/kits/preact-htm';
 import Icon from './Icon.js';
 
-export default function Taplet ({ icon, size, title, onClick }) {
+function Taplet ({ icon, size, title, onClick }) {
   return html`
     <button class="taplet" title=${title} onClick=${onClick}>
       <${Icon} name=${icon} size=${size} />
     </button>
   `;
 }
+
+export       { Taplet };
+export default Taplet;
