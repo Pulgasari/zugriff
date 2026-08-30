5edc0040f2200f723ab6bd8bbf1bfc2d4daaf376
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-30 18:25:19 +0200

    Update GhostButton.js

--- .shared/js/components/GhostButton.js
+++ .shared/js/components/GhostButton.js
@@ -1,12 +1,8 @@
 // shared/js/components/GhostButton.js
 
-import { html } from '@aufbau/kits/preact-htm';
-import Icon from './Icon.js';
+import Button from './Button.js';
 
-export default function GhostButton ({ children, icon, label, text, onClick, title }) {
-  return html`
-    <button class='btn ghost ghost-btn' onClick=${onClick} title=${title}>
-      ${icon && html`<${Icon} name=${icon} />`}
-      ${children || label || text}
-    </button>`;
+export default function GhostButton (props) {
+  props.className = 'ghost';
+  return Button(props);
 }
