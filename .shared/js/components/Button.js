574ad1c5c827ed4cd8e32edb008ae88123c6b0ed
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-30 18:37:54 +0200

    Update Button.js

--- .shared/js/components/Button.js
+++ .shared/js/components/Button.js
@@ -3,10 +3,13 @@
 import { html } from '@aufbau/kits/preact-htm';
 import Icon from './Icon.js';
 
-export default function Button ({ children, className, icon, onClick, label, text, disabled, title }) {
+function Button ({ children, className, icon, onClick, label, text, disabled, title }) {
   return html`
     <button class=${className} onClick=${onClick} disabled=${disabled} title=${title}>
       ${icon && html`<${Icon} name=${icon} />`}
       ${children || label || text}
     </button>`;
 }
+
+export       { Button };
+export default Button;
