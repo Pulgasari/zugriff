d676b2890a54678cc6c2a996b2109931d9cb1406
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-01 19:01:37 +0200

    Update config.js

--- .shared/js/app/config.js
+++ .shared/js/app/config.js
@@ -1,22 +1,9 @@
 // .shared/js/app/config.js
 
-import { registry } from './../registry.js';
+import { registry } from './../data/apps.js';
 
 const slug   = new URL(import.meta.url).searchParams.get('slug');
 const config = (slug && registry.get(slug)) || {};
-
-const config = {};
-
-function applyFont (id) {
-  if (id) aufbau.webfonts.load(id).then(ok => { 
-    if (ok) aufbau.webfonts.apply({ name: id, target: '--font' }); 
-  });
-}
-
-function applyDir (value) {
-  if (typeof document === 'undefined') return;
-  document.documentElement.dir = value || 'ltr';
-}
 
 // :::::: EXPORT
 
