667afce73d3d271586a2ab76e7bd510cb77c6af7
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-01 15:14:22 +0200

    Update state.js

--- .shared/js/app/state.js
+++ .shared/js/app/state.js
@@ -11,20 +11,36 @@ const $root = (typeof document !== 'undefined') ? document.documentElement : nul
 const state = {
   dialog : null,
   route  : null,
+  font   : 'Manrope',
 };
 
 // :::::: EFFECTS
 
 effect(() => $root?.setAttribute('dir', state.route.value));
 
-effect(() => {
-  const name = state.font.value;
-  aufbau.webfonts.load(name).then(ok => {
-    if (ok) aufbau.webfonts.apply({ name, target: '--font' }); 
-  });
-});
+effect(() => aufbau.webfonts.init({ name: state.font.value, target: '--font' }));
 
 // :::::: EXPORT
 
 export       { state };
 export default state;
+
+
+
+
+function onUpdate (key, callback) {
+  effect(() => {
+    const value = state[key].value;
+    callback(value);
+  });
+}
+
+effect(() => $root?.setAttribute('dir', state.route.value));
+
+onUpdate('dir',  (value) => $root?.setAttribute('dir', value));
+onUpdate('font', (value) => aufbau.webfonts.init({ name: value, target: '--font' }));
+
+onUpdate2({
+  key  : (value) => $root?.setAttribute('dir', value),
+  font : (value) => aufbau.webfonts.init({ name: value, target: '--font' })
+});
