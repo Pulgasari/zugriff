4d31608cb3e5d22dc9ccbbc57bd36f4d3000a1f8
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-01 14:47:30 +0200

    Update app.js

--- .shared/js/app/app.js
+++ .shared/js/app/app.js
@@ -10,3 +10,24 @@ const app = { config, state };
 
 export       { app };
 export default app;
+
+
+app.setDir  = value => app.state.dir  = value;
+app.setFont = id    => app.state.font = id;
+
+app.setDialog = id => app.state.dialog = id;
+app.setRoute  = id => app.state.route  = id;
+
+app.togglePanel = id => 
+
+
+function applyFont (id) {
+  if (id) aufbau.webfonts.load(id).then(ok => { c
+    if (ok) aufbau.webfonts.apply({ name: id, target: '--font' }); 
+  });
+}
+
+function applyDir (value) {
+  if (typeof document === 'undefined') return;
+  document.documentElement.dir = value || 'ltr';
+}
