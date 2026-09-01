d31bb4db102bffc50b37cd696c3b9aa10858aa74
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-01 17:02:35 +0200

    Update state.js

--- .shared/js/app/state.js
+++ .shared/js/app/state.js
@@ -19,51 +19,12 @@ const state = deepSignal({
 
 // :::::: EFFECTS
 
-effect(() => $root?.setAttribute('dir', state.route.value));
-
-effect(() => aufbau.webfonts.init({ name: state.font.value, target: '--font' }));
-
-state.onEffects ({
+state.$onEffects ({
   dir  : (value) => $root?.setAttribute('dir', value),
   font : (value) => aufbau.webfonts.init({ name: value, target: '--font' }),
 });
-
 
 // :::::: EXPORT
 
 export       { state };
 export default state;
-
-
-
-
-function onUpdate (key, callback) {
-  effect(() => {
-    const value = state[key].value;
-    callback(value);
-  });
-}
-
-
-state.onEffect = function (key, callback) {
-  effect(() => {
-    if (!(key in state)) return;
-    const value = state[key];
-    // prevent callback internal signal reads from leaking into this effect
-    untracked(() => callback(value));
-  });
-}
-
-state.onEffects = function (listeners) {
-  Object.entries(listeners).forEach(([key, callback]) => onEffect(key, callback));
-}
-
-effect(() => $root?.setAttribute('dir', state.route.value));
-
-onUpdate('dir',  (value) => $root?.setAttribute('dir', value));
-onUpdate('font', (value) => aufbau.webfonts.init({ name: value, target: '--font' }));
-
-state.onEffects ({
-  key  : (value) => $root?.setAttribute('dir', value),
-  font : (value) => aufbau.webfonts.init({ name: value, target: '--font' })
-});
