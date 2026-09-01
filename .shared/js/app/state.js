34f7faf7f49f2b12527751623742cb0619f03a0c
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-01 17:58:28 +0200

    Update state.js

--- .shared/js/app/state.js
+++ .shared/js/app/state.js
@@ -10,6 +10,7 @@ const config = (slug && registry.get(slug)) || {};
 
 // :::::: REFS
 
+const $doc  = (typeof document !== 'undefined') ? document : null;    
 const $root = (typeof document !== 'undefined') ? document.documentElement : null;    
 
 // :::::: MAIN
@@ -25,8 +26,11 @@ const state = deepSignal({
 // :::::: EFFECTS
 
 state.$onEffects ({
-  dir  : (value) => $root?.setAttribute('dir', value),
-  font : (value) => aufbau.webfonts.init({ name: value, target: '--font' }),
+  dir   : (value) => $root?.setAttribute('dir', value),
+  font  : (value) => aufbau.webfonts.init({ name: value, target: '--font' }),
+  lang  : (value) => $root.lang = value,
+  theme : (value) => $root.dataset.theme = value,
+  title : (value) => $doc.title = value,
 });
 
 // :::::: EXPORT
