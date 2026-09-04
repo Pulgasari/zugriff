eb474f79cc06cc651e65588e47aeeac804d42a8e
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-04 18:20:01 +0200

    Update runtime.js

--- .shared/js/runtime.js
+++ .shared/js/runtime.js
@@ -4,7 +4,7 @@
 // this module (directly, or transitively via an app's db.js) installs it.
 
 // :::::: IMPORTS
-
+/*
 import * as components from './components/index.js';
 import registry        from './data/apps.js';
 import createApp       from './app.js';
@@ -27,34 +27,62 @@ zugriff.openPrompt     = components.openPrompt;
 zugriff.toggleSettings = components.toggleSettings;
 
 if (typeof globalThis !== 'undefined') globalThis.zugriff = zugriff;
+*/
+// ============ NEW ==========================================================
 
-// ::: NEW
+// :::::: IMPORTS
 
-zugriff.getApp = (slug) => new App (slug);
+import registry          from './data/apps.js';
+import { App }           from './app.js';
+import { FolderLibrary } from './filesystem/folders.js';
+import * as fsaccess     from './filesystem/fsaccess.js';
+import { opfs }          from './filesystem/opfs.js';
 
-function moduleProxy(path) {
-  return new Proxy({}, {
-    get(_, name) {
-      return import(`${path}/${name}.js`).then(m => m.default ?? m);
-    }
-  });
-}
+// import * as pwa  from './app/pwa.js';
+// import { toast } from './app/toast.js';
 
-const PATH_COMPS   = '/.shared/js/components';
-const PATH_VENDOR  = '/.shared/js/vendors.js';
+// :::::: CONSTS
 
-const importModule = (path) => import(path).then(m => m.default ?? m);
+const PATH_COMPS  = '/.shared/js/components';
+const PATH_VENDOR = '/.shared/js/vendors.js';
 
-zugriff.importComponent = (name) => importModule(`${PATH_COMPS}/${name}.js`);        
-zugriff.importVendor    = (name) => {};
+// :::::: METHODS
 
+//const loadModule    = (path)      => import(path).then(mod => mod.default ?? mod);
+const loadModule    = (path, sub) => import(path).then(mod => (sub ? mod[sub] : (mod.default ?? mod)));
+const loadComponent = (name, sub) => loadModule(`${PATH_COMPS}/${name}.js`, sub);        
+const loadVendor    = (name)      => {};
+
+// :::::: BUNDLE
+
+const zugriff = {
+  // namespaces
+  fs: Object.assign({ FolderLibrary }, fsaccess);
+  opfs,
+  registry,
+  toast,
+
+  // methods
+  loadComponent,
+  loadModule,
+  loadVendor,
+};
+
+zugriff.getApp         = (slug) => new App (slug);
+zugriff.openPrompt     = await loadComponent ('Prompt', 'openPrompt');
+zugriff.toggleSettings = await loadComponent ('Settings', 'toggleSettings');
+
+/*
 // usage
-const components = moduleProxy('/.shared/js/components');
+const zugriff = new ZugriffRuntime;
 
-const AppSettings = await zugriff.importComponent('AppSettings');
-const Icon        = await zugriff.importComponent('Icon');
+const AppSettings = await zugriff.loadComponent('AppSettings');
+const Icon        = await zugriff.loadComponent('Icon');
+*/
 
 // :::::: EXPORT
+
+if (typeof globalThis !== 'undefined') globalThis.zugriff = zugriff;
 
 export       { zugriff };
 export default zugriff;
