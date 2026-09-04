d864cd5e292f0089a6415a832da4c17114c394bd
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-04 17:46:42 +0200

    Update runtime.js

--- .shared/js/runtime.js
+++ .shared/js/runtime.js
@@ -8,6 +8,7 @@
 import * as components from './components/index.js';
 import registry        from './data/apps.js';
 import createApp       from './app.js';
+import { App }         from './app.js';
 import { toast }       from './app/toast.js';
 
 import { FolderLibrary } from './filesystem/folders.js';
@@ -26,6 +27,32 @@ zugriff.openPrompt     = components.openPrompt;
 zugriff.toggleSettings = components.toggleSettings;
 
 if (typeof globalThis !== 'undefined') globalThis.zugriff = zugriff;
+
+// ::: NEW
+
+zugriff.getApp = (slug) => new App (slug);
+
+function moduleProxy(path) {
+  return new Proxy({}, {
+    get(_, name) {
+      return import(`${path}/${name}.js`).then(m => m.default ?? m);
+    }
+  });
+}
+
+const PATH_COMPS   = '/.shared/js/components';
+const PATH_VENDOR  = '/.shared/js/vendors.js';
+
+const importModule = (path) => import(path).then(m => m.default ?? m);
+
+zugriff.importComponent = (name) => importModule(`${PATH_COMPS}/${name}.js`);        
+zugriff.importVendor    = (name) => {};
+
+// usage
+const components = moduleProxy('/.shared/js/components');
+
+const AppSettings = await zugriff.importComponent('AppSettings');
+const Icon        = await zugriff.importComponent('Icon');
 
 // :::::: EXPORT
 
