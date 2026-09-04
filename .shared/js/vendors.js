a9e3b4ef0afad4244cc0e4599a8b1b4674b1d331
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-04 23:25:35 +0200

    Update vendors.js

--- .shared/js/vendors.js
+++ .shared/js/vendors.js
@@ -7,11 +7,36 @@
 
 // ::: preact + htm + signals, bundled by the aufbau kit (externalises preact via
 //     the importmap, so this is the only instance)
-export { default as aufbau } from '@aufbau/kits/preact-htm';
-export * from '@aufbau/kits/preact-htm';   // html, preact, render, str, signal, computed, effect, Fragment, hooks
+//export { default as aufbau } from '@aufbau/kits/preact-htm';
+//export * from '@aufbau/kits/preact-htm';   // html, preact, render, str, signal, computed, effect, Fragment, hooks
+
+// aufbau
+export { default as aufbau } from '@aufbau/runtime';
 
 // ::: pulgasari utils
 export * from '@pulgasari/is';
 export * from '@pulgasari/obj';
 export * from '@pulgasari/str';
 export * from '@pulgasari/timing';
+
+
+// ::: preact + htm
+import htm from 'htm'; 
+import * as preactCore    from 'preact';
+import * as preactHooks   from 'preact/hooks';
+import * as preactSignals from '@preact/signals';
+
+const preact = { 
+  ...preactCore,
+  ...preactHooks,
+  ...preactSignals,
+};
+
+const html = htm.bind(preact.h);
+
+export * from 'preact';
+export * from 'preact/hooks';
+export * from '@preact/signals';
+export { htm, html, preact };
+
+
