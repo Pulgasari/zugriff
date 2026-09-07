a1ad8dd12f527b7be886f48b3fa0d77c69c5d07a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-07 13:24:01 +0200

    Create PodcastListRow.js

---
+++ apps/podcasts/components/PodcastListRow.js
@@ -0,0 +1,14 @@
+function PodcastListRow ({ podcast }) {
+  const eps = db.episodesByPodcast.value[podcast.id] ?? [];
+  return html`
+    <button class="pc-row" onClick=${() => go('podcast', podcast.id)}>
+      <${Art} src=${podcast.image} size=${56} />
+      <div class="pc-row-body">
+        <div class="pc-title">${podcast.title}</div>
+        <div class="pc-sub">${podcast.author ? podcast.author + ' · ' : ''}${eps.length} episode${eps.length === 1 ? '' : 's'}</div>
+      </div>
+      <div class="pc-row-date">${fmtDate(podcast.lastEpisodeAt)}</div>
+    </button>`;
+}
+
+export default PodcastListRow;
