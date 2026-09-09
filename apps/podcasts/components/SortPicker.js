80ca94231f04cc5e26d2d45fc31dd8b1a77f2825
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-09 11:25:14 +0200

    Delete apps/podcasts/components/SortPicker.js

--- apps/podcasts/components/SortPicker.js
+++
@@ -1,7 +0,0 @@
-import Picker from '/.shared/js/components/Picker.js';
-
-function SortPicker ({ value, options, onChange }) {
-  return html`<${Picker} look='segments' options=${options} value=${value} onChange=${onChange} />`;
-}
-
-export default SortPicker;
