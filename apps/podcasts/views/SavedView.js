a7c99d72a215cb895a0c856a9c75043176512561
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-09 10:46:36 +0200

    Update SavedView.js

--- apps/podcasts/views/SavedView.js
+++ apps/podcasts/views/SavedView.js
@@ -11,13 +11,15 @@ export default function SavedView () {
   const list = db.savedEpisodes.value;
 
   const empty = {
-    icon: 'bookmarks', title: 'Your list is empty',
-    hint: 'Tap the bookmark on any episode to keep it here.',
+    icon  : 'bookmarks',
+    title : 'Your list is empty',
+    hint  : 'Tap the bookmark on any episode to keep it here.',
   };
 
   return html`
     <${View}>
       <header><h1>Listen later</h1></header>
       <${EpisodesIndex} episodes=${list} showPodcast empty=${empty} />
-    </${View}>`;
+    </${View}>
+  `;
 }
