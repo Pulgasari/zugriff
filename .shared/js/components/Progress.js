1351f716349bdd9cfe8d3b9909d6b5b3c1d2d570
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-09 06:35:13 +0200

    Update Progress.js

--- .shared/js/components/Progress.js
+++ .shared/js/components/Progress.js
@@ -1,5 +1,5 @@
 
 
 export default Progess ({ value, ...rest }) {
-  return html`<aufbau-progress value=${value}></aufbau-progress>`;
+  return html`<aufbau-progress class='Progress' value=${value}></aufbau-progress>`;
 }
