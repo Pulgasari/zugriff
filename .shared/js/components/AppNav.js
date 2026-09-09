204dec21b21cf788080b77888b728c04b3b6fb42
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-09 04:29:24 +0200

    Update AppNav.js

--- .shared/js/components/AppNav.js
+++ .shared/js/components/AppNav.js
@@ -3,32 +3,27 @@
 //import { html } from './../vendors.js';
 import Button from './Button.js';
 
+/*
 const items = [
   { title: 'Episodes', icon: 'mdi:playlist-play',     route: 'episodes' },
   { title: 'Podcasts', icon: 'mdi:view-grid-outline', route: 'podcasts' },
   { title: 'Later',    icon: 'mdi:bookmark-outline',  route: 'episodes' },     
   { title: 'Settings', icon: 'settings',             dialog: 'settings' },
 ];
-
+*/
 
 function AppNavItem ({ icon, label, dialog, panel, route, onClick, ...rest }) {
   if (dialog) onClick = () => zugriff.app.toggleDialog(dialog);
   if (panel)  onClick = () => zugriff.app.togglePanel(panel);
   if (route)  onClick = () => zugriff.app.openRoute(route); // go(route)
   
-  return html`
-    <${Button} class='item' onClick=${onClick}>
-      <${Icon} name=${icon} /> <span>${label}</span>
-    </${Button}>
-  `;
+  return html`<${Button} class='AppNavItem' ...${{ icon, label, onClick, ...rest }} />`;
 }
 
 function AppNav ({ items, ...rest }) {
   return html`
-    <aside id='app-nav'>
-      <nav>
-        {items.forEach(AppNavItem)}
-      </nav>
+    <aside id='app-nav' class='AppNav' ...${rest}>
+      ${items.map(AppNavItem)}
     </aside>
   `;
 }
