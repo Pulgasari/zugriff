60fdbd1def54572bffdaf5396ebca9920018d408
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-10 05:25:58 +0200

    Update Dock.js

--- .shared/js/components/Dock.js
+++ .shared/js/components/Dock.js
@@ -16,13 +16,14 @@ function DockNavItem ({ icon, label, dialog, panel, route, onClick, ...rest }) {
   if (dialog) onClick = () => zugriff.app.toggleDialog(dialog);
   if (panel)  onClick = () => zugriff.app.togglePanel(panel);
   if (route)  onClick = () => zugriff.app.go(route); // go(route)
+  if (view)   onClick = () => zugriff.app.go(view);
   
-  return html`<${Button} class='DockNavItem' ...${{ icon, label, onClick, ...rest }} />`;
+  return html`<${Button} class='DockNavItem col' ...${{ icon, label, onClick, ...rest }} />`;
 }
 
 function Dock ({ items = [], ...rest }) {
   return html`
-    <aside id='app-dock' class='Dock' ...${rest}>
+    <aside class='Dock' id='app-dock' ...${rest}>
       <div>ich bin dumm</div>
       ${items.map(DockNavItem)}
     </aside>
