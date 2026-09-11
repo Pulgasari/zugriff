1ac3cc239712e435cfc576f88d94f4e3ec1a602f
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-11 05:11:19 +0200

    Update Image.js

--- .shared/js/components/Image.js
+++ .shared/js/components/Image.js
@@ -2,8 +2,14 @@
 
 import { html } from './../vendors.js';
 
+function btn (comp, rest) {
+  return rest.onClick ? html`<button class='naked'>${comp}</button>` : comp;
+}
+
 function Image ({ loading = 'lazy', src, ...rest }) {
-  return html`<img ...${{ loading, src, ...rest }} />`;
+  const comp = html`<img ...${{ loading, src, ...rest }} />`;
+  
+  return btn(comp, rest);
 }
 
 export       { Image };
