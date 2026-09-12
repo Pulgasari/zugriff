e339fa9448c8ffcf0ddba20a492ed189c8e72744
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-12 20:03:01 +0200

    Update EpisodesIndex.js

--- apps/podcasts/components/EpisodesIndex.js
+++ apps/podcasts/components/EpisodesIndex.js
@@ -1,5 +1,6 @@
 // apps/podcasts/components/EpisodesIndex.js
 
+import ActionMenu from '/.shared/js/components/ActionMenu.js';
 import Button     from '/.shared/js/components/Button.js';
 import Empty      from '/.shared/js/components/Empty.js';
 import Index      from '/.shared/js/components/Index.js';
