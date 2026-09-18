24536e6cd2eb0e73fce1c45ac0951b8733fbdc6e
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-18 06:03:29 +0200

    Update database.js

--- apps/podcasts/modules/database.js
+++ apps/podcasts/modules/database.js
@@ -20,20 +20,6 @@ import { fetchFeed, parseFeed } from './feed.js';
 const URL_PROXY_IMG = 'https://img.pulgasari.dev/?url={url}&w={w}';
 const URL_PROXY_RSS = 'https://api.allorigins.win/raw?url={url}';
 
-// :::::: HELPERS
-
-function hash (str = '') {
-  let h1 = 0xdeadbeef, h2 = 0x41c6ce57;
-  for (let i = 0; i < str.length; i++) {
-    const ch = str.charCodeAt(i);
-    h1 = Math.imul(h1 ^ ch, 2654435761);
-    h2 = Math.imul(h2 ^ ch, 1597334677);
-  }
-  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-  return (4294967296 * (2097151 & h2) + (h1 >>> 0)).toString(36);
-}
-
 // :::::: 
 
 const database = createDb('zugriff:podcasts');
