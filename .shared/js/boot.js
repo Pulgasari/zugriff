fa87b724b9a592408d140ab3a088c199d3ba9eff
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-19 05:05:32 +0200

    Update boot.js

--- .shared/js/boot.js
+++ .shared/js/boot.js
@@ -308,6 +308,7 @@ function applyTheme (theme) {
       "@pulgasari/canonicalmap" : `${pkg}/js/canonicalmap.js`,
       "@pulgasari/coerce" : `${pkg}/js/coerce.js`,
       "@pulgasari/hash"   : `${pkg}/js/hash.js`,
+      "@pulgasari/htx"    : `${pkg}/js-packages/htx/index.js`,
       "@pulgasari/is"     : `${jsr}/@pulgasari/is`,
       "@pulgasari/logger" : `${jsr}/@pulgasari/logger`,
       "@pulgasari/num"    : `${pkg}/js/num.js`,
