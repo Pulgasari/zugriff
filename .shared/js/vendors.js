063ff3185ec4eccdd1d66daeca82c05385f6fea5
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-19 05:08:49 +0200

    Update vendors.js

--- .shared/js/vendors.js
+++ .shared/js/vendors.js
@@ -1,6 +1,7 @@
 // .shared/js/vendors.js
 
 //
+import { createHtml }                  from '@pulgasari/htx';
 import { isArray, isObject, isString } from '@pulgasari/is';
 
 // ::: pulgasari utils
@@ -26,72 +27,9 @@ const preact = {
   signal, Signal, computed, effect, batch, untracked,
 };
 
-// :::::: CUSTOM H
-
-function addClass (classList, value) {
-  if (!value) return;
-  else if (isString(value)) classList.push(value);
-  else if  (isArray(value)) for (const item of value) addClass(classList, item);
-  else if (isObject(value)) for (const [className, enabled] of Object.entries(value)) if (enabled) classList.push(className);        
-}
-
-function setProp (props, key, value) {
-  if (key === 'class' || key === 'className' || key.startsWith('class:')) {
-    if (!props._classes) props._classes = [];
-    if (value != null && value !== false) {
-      props._classes.push({ key, value });
-    }
-  } 
-  else props[key] = value;
-}
-
-/* 
-Custom h function for preact/htm to support:
-- Fragment fallback for empty tags (<>...</>)
-- Merging multiple class and className attributes
-- Object syntax (e.g. class=${{ active: isTrue, disabled: false }})
-- Directive syntax (e.g. class:active=${isTrue})
-*/
-function customH (type, props, ...children) {
-  const targetType = type || Fragment;
-
-  if (!props) return h (targetType, props, ...children);
-
-  const newProps  = {};
-  const classList = [];
-
-  // 1. Process classes collected during htm prop parsing / spreading
-  if (props._classes) {
-    for (const { key, value } of props._classes) {
-      if (key.startsWith('class:')) {
-        const className = key.slice(6);
-        if (value) classList.push(className);
-      } 
-      else addClass(classList, value);
-    }
-  }
-
-  for (const [key, value] of Object.entries(props)) {
-    if (key === '_classes') continue;
-    if (key.startsWith('class:')) {
-      const className = key.slice(6);
-      if (value) classList.push(className);
-    } else if (key === 'class' || key === 'className') {
-      // Handle strings, objects, or arrays passed to class/className
-      addClass(classList, value);
-    } 
-    else newProps[key] = value; // preserve all other props
-    
-  }
-
-  // build classList
-  if (classList.length > 0) newProps.class = classList.join(' ');
-
-  // done!
-  return h (targetType, newProps, ...children);
-}
-
-const html = htm.bind(customH);
+// htx
+const html = createHtml(h, Fragment);
+const htx  = createHtml(h, Fragment);
 
 // :::::: EXPORT
 
