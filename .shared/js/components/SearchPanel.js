8985339b652813196870f101192171f761d8b909
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-21 15:09:50 +0200

    Update SearchPanel.js

--- .shared/js/components/SearchPanel.js
+++ .shared/js/components/SearchPanel.js
@@ -5,11 +5,21 @@ import SearchInput from './SearchInput.js';
 
 const app = zugriff.app;
 
-function SearchPanel ({ placeholder, appStateId: id = 'search' }) {
-  const clear   = ()      => app.state[id] = '';
-  const onInput = (event) => app.state[id] = event.detail.value;
-  const value   = app.state['$' + id];   // the leaf's value; the bare name is its signal
+function SearchPanel ({ placeholder, signal, appStateId: id = 'search' }) {
+  let clear, onInput, value;
 
+  if (signal !== undefined) {
+    clear   = ()      => signal.value = '';
+    onInput = (event) => signal.value = event.detail.value;
+    value   = signal.value;
+  }
+
+  else {
+    clear   = ()      => app.state[id] = '';
+    onInput = (event) => app.state[id] = event.detail.value;
+    value   = app.state['$' + id];   // the leaf's value; the bare name is its signal
+  }
+  
   return html`
     <div class='SearchPanel search-panel'>
       <${SearchInput} ...${{ onInput, placeholder, value }} />
