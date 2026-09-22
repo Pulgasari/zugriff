ef864c453f7f9a4a17b06e15537a26ccffda47aa
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-22 05:31:52 +0200

    Update View.js

--- .shared/js/components/View.js
+++ .shared/js/components/View.js
@@ -1,23 +1,16 @@
 // components/View.js
-// the outer frame every app view sits in: an optional back button, an optional
-// header (title + right-aligned tools), then the view body. list views pass
-// `title`/`tools`; detail views pass `back` and bring their own header as
-// children.
 
-import { html } from './../vendors.js';
-import Button   from './Button.js';
+import Button       from './Button.js';
+import GoBackButton from './GoBackButton.js';
 
 function View ({ title, back, tools, children, ...rest }) {
   return html`
-    <div class='View' ...${rest}>
-      ${back && html`<${Button} class='ViewBack' icon='arrow-left' ...${back} />`}
-
-      ${(title || tools) && html`
-        <header>
-          ${title && html`<h1>${title}</h1>`}
-          ${tools && html`<div class='ViewTools'>${tools}</div>`}
-        </header>`}
-
+    <div class='View view' ...${rest}>
+      <header>
+        ${back  && html`<${Button} class='ViewBack' icon='arrow-left' ...${back} />`}      
+        ${title && html`<h1>${title}</h1>`}
+        ${tools && html`<div class='ViewTools'>${tools}</div>`}
+      </header>
       ${children}
     </div>
   `;
